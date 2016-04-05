a50b9b29e27ea8199501360dcc3baedd7287d042
Parker Holcomb <parker.holcomb@gmail.com>
2016-04-05 08:56:30 -0400

    contact me on Facebook

--- js/my-scripts.js
+++ js/my-scripts.js
@@ -1,14 +1,8 @@
 $( document ).ready(function()
 {
 
-
     $("#contact").click(function(){
-
-      console.log("this is a log");
-      alert('this pops up on click');
-
+      alert('contact me on Facebook');
     });
-
-
 
 });
